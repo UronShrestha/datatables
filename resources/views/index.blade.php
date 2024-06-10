2c51e6e99e6a25d9a02562b3cc7954d057166ad9
UronShrestha <yuron.stha57@gmail.com>
2024-06-10 08:10:37 +0545

    Use Datatables .net library to show table - with our without server-side rendering

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.masterlayout')
+@section('main-content')
+    <div class="container">
+        <h1>{{ __('List of Users') }}</h1>
+        <div class="table-responsive">
+            <table id ="tbl" class="table table-bordered table-striped">
+                <thead class="table-dark">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Created At</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse($data as $row)
+                        <tr class="">
+                            <td scope="row">{{ $row->id }}</td>
+                            <td>{{ $row->name }}</td>
+                            <td>{{ $row->email }}</td>
+                            <td>{{ $row->created_at }}</td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td>No Users Found</td>
+                        </tr>
+                    @endforelse
+
+                </tbody>
+            </table>
+
+        </div>
+
+    </div>
+@endsection
+
+@push('js')
+    <script>
+        $(document).ready(function() {
+            $("#tbl").DataTable();
+        });
+    </script>
+@endpush
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.css">
+    <title>Datatables</title>
+</head>
